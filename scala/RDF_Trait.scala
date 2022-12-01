d6dbe287678193f4f2b66c861f7ef125cb0e3a8a
Henry Story <henry.story@bblfish.net>
2022-12-01 15:50:11 +0100

    minor fix

--- scala/RDF_Trait.scala
+++ scala/RDF_Trait.scala
@@ -5,16 +5,16 @@ import scala.reflect.TypeTest
 
 object TraitTypes {
   trait Node:
-     def value: String
+    def value: String
 
   trait Uri extends Node
-  
+
   trait Factory:
-     def mkUri(u: String): Uri
-  
+    def mkUri(u: String): Uri
+
   object aFactory extends Factory:
-     def mkUri(u: String): Uri = 
-       new Uri { def value = u }
+    def mkUri(u: String): Uri =
+      new Uri { def value = u }
 }
 
 object TraitBasedRDF extends generic.RDF:
@@ -22,8 +22,8 @@ object TraitBasedRDF extends generic.RDF:
   import interf_based.TraitTypes as tz
 
   override opaque type rNode <: Matchable = tz.Node
-  override opaque type rURI <: rNode  = tz.Uri
-  override opaque type Node <: rNode = tz.Node 
+  override opaque type rURI <: rNode = tz.Uri
+  override opaque type Node <: rNode = tz.Node
   override opaque type URI <: Node & rURI = tz.Uri
 
   given rops: generic.ROps[R] with
@@ -31,11 +31,11 @@ object TraitBasedRDF extends generic.RDF:
       tz.aFactory.mkUri(str)
     )
     override protected def nodeVal(node: RDF.Node[R]): String = node.value
-    override def auth(uri: RDF.URI[R]): Try[String] = 
+    override def auth(uri: RDF.URI[R]): Try[String] =
       Try(java.net.URI.create(nodeVal(uri)).getAuthority())
- 
+
 end TraitBasedRDF
 
 @main def run =
-  val test = generic.Test[TraitRDF.type]
+  val test = generic.Test[TraitBasedRDF.type]
   println(test.x)
