97dea61e5ad3ef30cac831ac14ae681cdbe859e1
Henry Story <henry.story@bblfish.net>
2023-06-24 16:14:20 +0200

    unreachable case

--- scala/RDF.scala
+++ scala/RDF.scala
@@ -1,4 +1,6 @@
 import scala.util.Try
+import scala.reflect.TypeTest
+
 
 trait RDF:
   rdf =>
@@ -6,6 +8,7 @@ trait RDF:
   type R = rdf.type
   type Node <: Matchable
   type URI <: Node 
+  type BNode <: Node 
 
   given rops: ROps[R]
 end RDF
@@ -17,23 +20,30 @@ object RDF:
   type URI[R <: RDF] <: Node[R] = R match
     case GetURI[u] => u & Node[R] 
 
+  type BNode[R <: RDF] <: Node[R] = R match
+    case GetBNode[b] => b & Node[R] 
+
+  type Subject[R <: RDF] = URI[R] | BNode[R]  
+
   private type GetNode[N] = RDF { type Node = N }
   private type GetURI[U] = RDF { type URI = U }
+  private type GetBNode[B] = RDF { type BNode = B }
 end RDF
 
 trait ROps[R <: RDF]:
   def mkUri(str: String): Try[RDF.URI[R]]
   def auth(uri: RDF.URI[R]): Try[String]
+  given subjToURITT: TypeTest[RDF.Subject[R], RDF.URI[R]]
 
 object TraitTypes:
   trait Node:
     def value: String
 
   trait Uri extends Node
-
+  trait BNode extends Node
+  
   def mkUri(u: String): Uri =
     new Uri { def value = u }
-
 
 object TraitRDF extends RDF:
   import TraitTypes as tz
@@ -46,6 +56,12 @@ object TraitRDF extends RDF:
     override def auth(uri: RDF.URI[R]): Try[String] =
       Try(java.net.URI.create(uri.value).getAuthority())
 
+    given subjToURITT: TypeTest[RDF.Subject[R], RDF.URI[R]] with
+       override def unapply(s: RDF.Subject[R]): Option[s.type & URI] =
+         s match
+           case x: (s.type & RDF.URI[R]) => Some(x)
+           case _                        => None
+  
 end TraitRDF
 
 class Test[R <: RDF](using rops: ROps[R]):
